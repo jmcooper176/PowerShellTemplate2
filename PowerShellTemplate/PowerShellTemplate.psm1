c76f4bf9ed1ad788f3ae53e1609b9c6bf34cac9e
John Merryweather Cooper <jmcooper8654@gmail.com>
2024-06-22 19:26:46 -0500

    Update unit tests to work

--- PowerShellTemplate/PowerShellTemplate.psm1
+++ PowerShellTemplate/PowerShellTemplate.psm1
@@ -3,15 +3,63 @@
 #>
 function Get-Function {
 	[CmdletBinding()]
-	param ()
+	param (
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]
+		$Value
+	)
 
-	if ($PSVersionTable.Major -gt 5) {
+	if ($PSVersionTable.PSVersion.Major -gt 5) {
 		Set-StrictMode -Version latest
-	} elseif ($PSVersionTable -ge 3) {
+	} elseif ($PSVersionTable.PSVersion.Major -ge 3) {
 		Set-StrictMode -Version 3.0
 	} else {
 		Set-StrictMode -Version 2.0
 	}
 
 	Set-Variable -Name CmdletName -Option ReadOnly -Value $MyInvocation.MyCommand.Name
+
+	$Value | Write-Output
+
+	<#
+		.SYNOPSIS
+		Echoes 'Value' to the output pipeline.
+
+		.DESCRIPTION
+		`Get-Function` echoes 'Value' to the output pipeline.
+
+		.PARAMETER Value
+		Specifies the string value to be echoed.  This parameter is mandatory.
+		This parameter cannot be null or an empty string.
+
+		.INPUT
+		None.  `Get-Function` receives no input from the pipeline.
+
+		.OUTPUT
+		[string]  `Get-Function outputs a string to the pipeline.
+
+		.EXAMPLE
+		PS> Get-Function 'Value'
+
+		Value
+
+		.NOTES
+		None.
+
+		.LINK
+		about_Advanced_Functions
+
+		.LINK
+		about_CommonParameters
+
+		.LINK
+		Set-StrictMode
+
+		.LINK
+		Set-Variable
+
+		.LINK
+		Write-Output
+	#>
 }
--- PowerShellTemplate/PowerShellTemplate.tests.ps1
+++ PowerShellTemplate/PowerShellTemplate.tests.ps1
@@ -1,13 +1,88 @@
 #
 # This is a PowerShell Unit Test file.
-# You need a unit test framework such as Pester to run PowerShell Unit tests. 
+# You need a unit test framework such as Pester to run PowerShell Unit tests.
 # You can download Pester from https://go.microsoft.com/fwlink/?LinkID=534084
 #
 
+<#
+	.SYNOPSIS
+	Runs Pester unit tests.
+
+	.DESCRIPTION
+	`PowerShellTemplate.tests.ps1` runs Pester unit tests.
+
+	.INPUT
+	None.
+
+	.OUTPUT
+	None.
+
+	.EXAMPLE
+
+	.NOTES
+	None.
+#>
+
+
 Describe "Get-Function" {
 	Context "Function Exists" {
-		It "Should Return" {
-		
+		BeforeAll {
+			# Arrange
+			$modulePath = Join-Path -Path $PWD -ChildPath PowerShellTemplate.psd1
+			Import-Module -Name $modulePath -Verbose -Force
+		}
+
+		It "Module Path should exist" {
+			# Act and Assert
+			Test-Path -LiteralPath $modulePath -PathType Leaf | Should -BeTrue
+		}
+
+		It "Should Import" {
+			# Arrange
+			$functionPath = Join-Path -Path function: -ChildPath 'Get-Function'
+
+			# Act and Assert
+			Test-Path -LiteralPath $functionPath | Should -BeTrue
+		}
+	}
+
+	Context "Function Performs as Designed" {
+		BeforeAll {
+			# Arrange
+			$modulePath = Join-Path -Path $PWD -ChildPath PowerShellTemplate.psd1
+			Import-Module -Name $modulePath -Verbose -Force
+		}
+
+		It "Should Return the Same 'Value' Passed as a Parameter" {
+			# Arrange
+			$expected = 'Test'
+
+			# Act and Assert
+			Get-Function -Value $expected | Should -BeExactly $expected
+		}
+
+		It "Should Return the Same 'Value' with spaces Passed as a Parameter" {
+			# Arrange
+			$expected = 'This is a Test'
+
+			# Act and Assert
+			Get-Function -Value $expected | Should -BeExactly $expected
+		}
+
+		It "Should throw on null" {
+			# Arrange
+			$test = $null
+
+			# Act and Assert
+			{ Get-Function -Value $test } | Should -Throw
+		}
+
+		It "Should throw on empty string" {
+			# Arrange
+			$test = ''
+
+			# Act and Assert
+			{ Get-Function -Value $test } | Should -Throw
 		}
 	}
 }
